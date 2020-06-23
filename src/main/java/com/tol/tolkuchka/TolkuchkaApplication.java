d63126980732372b83e77b5a3c055f017bcd878e
alex <chernulich.alex@gmail.com>
2020-06-24 00:57:25 +0300

    second commit

---
+++ src/main/java/com/tol/tolkuchka/TolkuchkaApplication.java
@@ -0,0 +1,13 @@
+package com.tol.tolkuchka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolkuchkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TolkuchkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tol/tolkuchka/TolkuchkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tol.tolkuchka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TolkuchkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
